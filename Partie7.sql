b39f5c93b1fbf24dca91f09fda3b232ad355f98f
AS <albrecht.stephanie@outlook.fr>
2020-01-22 11:50:31 +0100

    correction partie7

--- Partie7.sql
+++ Partie7.sql
@@ -2,12 +2,11 @@
 
 --Exercice1 : Dans la table languages, supprimer toutes les lignes parlant de HTML.
 
-SELECT  * FROM `languages`  WHERE language != 'HTML';
+DELETE FROM languages WHERE language LIKE '%HTML%';
 
 --Exercice2 : Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
 
-UPDATE `frameworks` SET framework = REPLACE(framework, 'Symfony','Symfony2');
---UPDATE `frameworks` SET framework = 'Symphony' WHERE framework = 'Symfony2';
+UPDATE `frameworks` SET framework = 'Symphony2' WHERE framework = 'Symfony';
 
 --Exercice3: Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1
 
